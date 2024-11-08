00b88f94059facb1bd3443a0641f5dfa92d3d5eb
Bryan <franciscobryaaan@gmail.com>
2024-11-08 23:06:18 +0800

    fix player direction to cam

--- player.gd
+++ player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody3D
 
 
-#const SPEED = 5.0
-#const JUMP_VELOCITY = 4.5
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+@onready var player_mesh: Node3D = $Mesh
 
 @onready var spring_arm: SpringArm3D = $"Camroot/TPS Offset/SpringArm3D"
 @onready var camroot: Node3D = $Camroot
@@ -24,24 +26,26 @@ func _input(event):
 func _physics_process(delta: float) -> void:
 	camroot.rotation_degrees.x = camrot_v
 	camroot.rotation_degrees.y = camrot_h
+	player_mesh.rotation_degrees.y = camrot_h
+	
 	
 	#Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-#
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-#
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var input_dir := Input.get_vector("left", "right", "forward", "backward")
-	#var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	#if direction:
-		#velocity.x = direction.x * SPEED
-		#velocity.z = direction.z * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-		#velocity.z = move_toward(velocity.z, 0, SPEED)
-#
-	#move_and_slide()
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("left", "right", "forward", "backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
