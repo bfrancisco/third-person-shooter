cb9855e9da6221052ea25d338866a92a8e20894e
franch1se <ronald.francis.bautista@student.ateneo.edu>
2024-11-12 03:16:38 +0800

    shooting fix

--- projectile.gd
+++ projectile.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 @export var SPEED = 0.4
 var target_position: Vector3 = Vector3.ZERO
-var direction
+var direction: Basis = Basis.IDENTITY
 
 func _ready() -> void:
 	look_at(target_position)
@@ -13,8 +13,8 @@ func _process(delta: float) -> void:
 	pass
 	
 func _physics_process(delta: float) -> void:
-	global_position += global_basis * Vector3.FORWARD * SPEED
+	global_position += direction * Vector3.FORWARD * SPEED
 	
 func _on_area_3d_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
+	print('collided with ', body.name)
 	queue_free()
-	print('collided')
--- rifle.gd
+++ rifle.gd
@@ -13,4 +13,5 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("shoot"):
 		var bullet = projectile.instantiate()
 		bullet.target_position = target_pos
+		bullet.direction = global_basis
 		add_child(bullet)
