893539212d1ed29a698a5c3f02b0a148f8e03b5a
franch1se <ronald.francis.bautista@student.ateneo.edu>
2024-11-12 03:34:48 +0800

    minor fix

--- projectile.gd
+++ projectile.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 
-@export var SPEED = 0.5
+@export var SPEED = 50
 var target_position: Vector3 = Vector3.ZERO
 var direction: Basis = Basis.IDENTITY
 
@@ -8,7 +8,7 @@ func _ready() -> void:
 	pass
 	
 func _physics_process(delta: float) -> void:
-	global_position += direction * Vector3.FORWARD * SPEED
+	global_position += direction * Vector3.FORWARD * (delta * SPEED)
 	
 func _on_area_3d_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
 	print('collided with ', body.name)
